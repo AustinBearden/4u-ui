dbf134d0ed5d6179c1bba8d42ed2dce6329d4a56
aibearde <aibearde@iupui.edu>
2021-10-04 17:07:26 -0400

    Building basic template one

--- src/components/Layout1.jsx
+++ src/components/Layout1.jsx
@@ -3,7 +3,31 @@ import React from 'react';
 function Layout1() {
 
   return (
-    <div>this is layout 1</div>
+    <div className={'layout-one'}>
+      <header>
+        <div className={'left-aligned-items'}>
+          hey
+        </div>
+        <div className={'right-aligned-items'}>
+          buddy
+        </div>
+      </header>
+      <body>
+        <div className={'left-drawer'}>hey</div>
+        <div className={'main-content'}>there</div>
+      </body>
+      <footer>
+        <div className={'left-content'}>
+          left content
+        </div>
+        <div className={'center-content'}>
+          center content
+        </div>
+        <div className={'right-content'}>
+          right content
+        </div>
+      </footer>
+    </div>
   );
 }
 
