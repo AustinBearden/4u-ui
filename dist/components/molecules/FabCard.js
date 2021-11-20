0f3d5e77139f89575040294fdb9d6c3d24d00104
aibearde <austinb99@4jlt.com>
2021-11-20 15:26:53 -0500

    small updateds

--- dist/components/molecules/FabCard.js
+++ dist/components/molecules/FabCard.js
@@ -9,12 +9,11 @@ var _react = _interopRequireDefault(require("react"));
 
 var _Button = _interopRequireDefault(require("../atoms/Button"));
 
-var _ViewWeek = _interopRequireDefault(require("@mui/icons-material/ViewWeek"));
-
 var _jsxRuntime = require("react/jsx-runtime");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// import ViewWeekIcon from '@mui/icons-material/ViewWeek';
 function FabCard(_ref) {
   let {
     bodyText,
@@ -28,13 +27,13 @@ function FabCard(_ref) {
     }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
       className: 'fab-card-end',
       children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-        className: 'menu',
-        children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_ViewWeek.default, {})
+        className: 'menu'
       }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
         className: 'menu-expanded',
         children: buttons.map((button, _) => {
           return /*#__PURE__*/(0, _jsxRuntime.jsx)(_Button.default, {
-            value: button.value
+            value: button.value,
+            action: () => alert(button.value)
           });
         })
       })]
--- dist/index.js
+++ dist/index.js
@@ -9,8 +9,32 @@ Object.defineProperty(exports, "Button", {
     return _Button.default;
   }
 });
+Object.defineProperty(exports, "FabCard", {
+  enumerable: true,
+  get: function get() {
+    return _FabCard.default;
+  }
+});
+Object.defineProperty(exports, "OptionCircle", {
+  enumerable: true,
+  get: function get() {
+    return _OptionCircle.default;
+  }
+});
+Object.defineProperty(exports, "SideBar", {
+  enumerable: true,
+  get: function get() {
+    return _SideBar.default;
+  }
+});
 
 var _Button = _interopRequireDefault(require("./components/atoms/Button"));
+
+var _FabCard = _interopRequireDefault(require("./components/molecules/FabCard"));
+
+var _OptionCircle = _interopRequireDefault(require("./components/molecules/OptionCircle"));
+
+var _SideBar = _interopRequireDefault(require("./components/molecules/SideBar"));
 
 require("./styles/main.css");
 
